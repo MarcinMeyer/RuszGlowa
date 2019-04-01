14b02d0da24165c3c0f5b45a3f581e468660ebe4
Marcin Meyer <marcin.meyer.poczta@gmail.com>
2019-04-01 19:17:33 +0200

    str 74 odp A

--- src/puzzle/BadzJakKompilator.java
+++ src/puzzle/BadzJakKompilator.java
@@ -2,13 +2,15 @@ package puzzle;
 
 
 public class BadzJakKompilator {
-    public static void startPuzzle() {
+    public static void startPuzzle() { // w zadaniach imituje main
 
+        Magnetofon m = new Magnetofon();
+        m.mozeNagrywac = true;
+        m.odtworzTasme();
 
-
-
-
-
+        if (m.mozeNagrywac == true) {
+            m.nagrajTasme();
+        }
 
 
     }
---
+++ src/puzzle/Magnetofon.java
@@ -0,0 +1,13 @@
+package puzzle;
+
+public class Magnetofon {
+    boolean mozeNagrywac = false;
+
+    void odtworzTasme() {
+        System.out.println("odtwarzam taśmę");
+    }
+
+    void nagrajTasme() {
+        System.out.println("nagrywam taśmę");
+    }
+}
