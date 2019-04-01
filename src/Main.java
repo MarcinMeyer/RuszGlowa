8fab4cc6731f015afb9b529d38dc8dc66c395e12
Marcin Meyer <marcin.meyer.poczta@gmail.com>
2019-04-01 20:09:21 +0200

    str 76 Zagadkowy basen

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,23 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Str 76");
 
-
+        Echo e1 = new Echo();
+        Echo e2 = new Echo();
+        int x = 0;
+        while (x < 4) {
+            e1.witaj();
+            e1.ilosc = e1.ilosc + 1;
+            if (x == 4) {
+                e2.ilosc = e2.ilosc + 1;
+            }
+            if (x < 4) {
+                e2.ilosc = e2.ilosc + e1.ilosc;
+            }
+            x = x + 1;
+        }
+        System.out.println(e2.ilosc);
 
     }
 }
