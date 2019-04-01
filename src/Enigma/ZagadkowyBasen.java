27a9cc1d3ce859e3987ce2cc329d61b73333f64d
Marcin Meyer <marcin.meyer.poczta@gmail.com>
2019-04-01 17:04:25 +0200

    Add new files

---
+++ src/Enigma/ZagadkowyBasen.java
@@ -0,0 +1,52 @@
+package Enigma;
+
+public class ZagadkowyBasen {// Strona 56 Rozdział 1
+
+    public static void zagadkowyBasen1() { // moje rozwiązanie
+        int x = 0;
+        while (x < 4) {
+            System.out.print("a");
+            if (x < 1) {
+                System.out.print(" ");
+            }
+            System.out.print("n");
+            if (x < 1) {
+                System.out.print("asz");
+                x = x - 1;
+            }
+            if (x == 1) {
+                System.out.print("talek");
+            }
+            if (x > 1) {
+                System.out.print("tyczny");
+            }
+            System.out.println(" ");
+            x = x + 2;
+
+        }
+    }
+
+    public static void zagadkowyBasen2() { // podręcznikowe rozwiązanie dla porównania
+        int x = 0;
+        while (x < 4) {
+            System.out.print("a");
+            if (x < 1) {
+                System.out.print(" ");
+            }
+            System.out.print("n");
+            if (x > 1) {
+                System.out.print("tyczny");
+                x = x + 2;
+            }
+            if (x == 1) {
+                System.out.print("talek");
+            }
+            if (x < 1) {
+                System.out.print("asz");
+            }
+            System.out.println(" ");
+            x = x + 1;
+
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import static Enigma.ZagadkowyBasen.zagadkowyBasen1;
+import static Enigma.ZagadkowyBasen.zagadkowyBasen2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // Zad 1
+        System.out.println("___________________________________________ ");
+        System.out.println("Str 56 - Zagadkowy Basen - moje rozwiązanie");
+        zagadkowyBasen1();
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - - ");
+        System.out.println("Dla porównania, odp z podręcznika");
+        zagadkowyBasen2();
+        System.out.println("___________________________________________ ");
+
+        // Zad2
+
+
+    }
+}
