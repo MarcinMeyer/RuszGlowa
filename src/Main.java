8da1e571548e4f8053afab04e87bd4ae01561d0f
Marcin Meyer <marcin.meyer.poczta@gmail.com>
2019-04-01 19:42:54 +0200

    clear everything

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-import static enigma.ZagadkowyBasen.startEnigma;
-import static puzzle.BadzJakKompilator.startPuzzle;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        startPuzzle();
-        startEnigma();
+
 
     }
 }
